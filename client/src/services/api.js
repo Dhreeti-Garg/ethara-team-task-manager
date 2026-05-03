3afdd2bc1cf25093c57d7a9e23cdf7b1486235fc
Dhreeti-Garg <dhreetigarg0505@gmail.com>
2026-05-03 17:13:58 +0530

    Update API base URL to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,7 @@
-// src/services/api.js
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Automatically attach JWT token
